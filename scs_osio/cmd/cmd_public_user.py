8bcd0881305a71dbd1f87c8c5564a298fbf1e303
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-04-30 13:17:04 +0100

    Added UserMetadata and UserTopic classes.

---
+++ scs_osio/cmd/cmd_public_user.py
@@ -0,0 +1,63 @@
+"""
+Created on 30 Apr 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+import optparse
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class CmdPublicUser(object):
+    """
+    unix command line handler
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.__parser = optparse.OptionParser(usage="%prog USER_ID [-v]", version="%prog 1.0")
+
+        # optional...
+        self.__parser.add_option("--verbose", "-v", action="store_true", dest="verbose", default=False,
+                                 help="report narrative to stderr")
+
+        self.__opts, self.__args = self.__parser.parse_args()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def is_valid(self):
+        if self.user_id is None:
+            return False
+
+        return True
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def user_id(self):
+        return self.__args[0] if len(self.__args) > 0 else None
+
+
+    @property
+    def verbose(self):
+        return self.__opts.verbose
+
+
+    @property
+    def args(self):
+        return self.__args
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def print_help(self, file):
+        self.__parser.print_help(file)
+
+
+    def __str__(self, *args, **kwargs):
+        return "CmdPublicUser:{user_id:%s, verbose:%s, args:%s}" % (self.user_id, self.verbose, self.args)
--- scs_osio/public_user.py
+++ scs_osio/public_user.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python3
 
 """
-Created on 21 Mar 2017
+Created on 30 Apr 2017
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
-Requires APIAuth and ClientAuth documents.
+Requires APIAuth document.
 
 command line example:
-./user.py -v -n "Mickey Mouse"
+./public_user.py -v south-coast-science-test-user
 """
 
 import sys
 
 from scs_core.data.json import JSONify
 from scs_core.osio.client.api_auth import APIAuth
-from scs_core.osio.client.client_auth import ClientAuth
-from scs_core.osio.data.user import User
 from scs_core.osio.manager.user_manager import UserManager
 
 from scs_host.client.http_client import HTTPClient
 from scs_host.sys.host import Host
 
-from scs_osio.cmd.cmd_user import CmdUser
+from scs_osio.cmd.cmd_public_user import CmdPublicUser
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -32,7 +30,7 @@ if __name__ == '__main__':
     # ----------------------------------------------------------------------------------------------------------------
     # cmd...
 
-    cmd = CmdUser()
+    cmd = CmdPublicUser()
 
     if not cmd.is_valid():
         cmd.print_help(sys.stderr)
@@ -54,16 +52,6 @@ if __name__ == '__main__':
 
     if cmd.verbose:
         print(api_auth, file=sys.stderr)
-
-    # ClientAuth...
-    client_auth = ClientAuth.load_from_host(Host)
-
-    if client_auth is None:
-        print("ClientAuth not available.", file=sys.stderr)
-        exit()
-
-    if cmd.verbose:
-        print(client_auth, file=sys.stderr)
         sys.stderr.flush()
 
     # manager...
@@ -74,21 +62,7 @@ if __name__ == '__main__':
     # run...
 
     # find self...
-    user = manager.find(client_auth.user_id)
+    user = manager.find_public(cmd.user_id)
 
-    if user is None:
-        print("User not found.", file=sys.stderr)
-        exit()
-
-    if cmd.set():
-        name = user.name if cmd.name is None else cmd.name
-        email = user.email if cmd.email is None else cmd.email
-        password = user.password if cmd.password is None else cmd.password
-
-        updated = User(None, name, email, password, None)
-
-        manager.update(user.id, updated)
-
-        user = manager.find(client_auth.user_id)
-
-    print(JSONify.dumps(user))
+    if user:
+        print(JSONify.dumps(user))
