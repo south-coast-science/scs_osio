61324fd333120b043ed7f8f951be37ffb24cd27c
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-05-21 12:54:06 +0100

    Added csv_reader and csv_writer to scs_osio.

---
+++ scs_osio/csv_writer.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+"""
+Created on 19 Aug 2016
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+command line example:
+./socket_receiver.py | ./csv_writer.py temp.csv -e
+"""
+
+import sys
+
+from scs_core.csv.csv_writer import CSVWriter
+from scs_core.data.json import JSONify
+from scs_core.sys.exception_report import ExceptionReport
+
+from scs_osio.cmd.cmd_csv_writer import CmdCSVWriter
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    cmd = None
+    csv = None
+
+    try:
+        # ------------------------------------------------------------------------------------------------------------
+        # cmd...
+
+        cmd = CmdCSVWriter()
+
+        if cmd.verbose:
+            print(cmd, file=sys.stderr)
+            sys.stderr.flush()
+
+
+        # ------------------------------------------------------------------------------------------------------------
+        # resources...
+
+        csv = CSVWriter(cmd.filename, cmd.cache, cmd.append)
+
+        if cmd.verbose:
+            print(csv, file=sys.stderr)
+
+
+        # ------------------------------------------------------------------------------------------------------------
+        # run...
+
+        for line in sys.stdin:
+            datum = line.strip()
+
+            if datum is None:
+                break
+
+            csv.write(datum)
+
+            # echo...
+            if cmd.echo:
+                print(datum)
+                sys.stdout.flush()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # end...
+
+    except KeyboardInterrupt as ex:
+        if cmd.verbose:
+            print("csv_writer: KeyboardInterrupt", file=sys.stderr)
+
+    except Exception as ex:
+        print(JSONify.dumps(ExceptionReport.construct(ex)), file=sys.stderr)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # close...
+
+    finally:
+        if csv is not None:
+            csv.close()
