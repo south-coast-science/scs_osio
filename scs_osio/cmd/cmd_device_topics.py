d6e66510fd60fa220835e6ee8bdfbb1f74c1e2a3
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-07-02 13:12:45 +0100

    Added device_topics top-level script.

---
+++ scs_osio/cmd/cmd_device_topics.py
@@ -0,0 +1,103 @@
+"""
+Created on 2 Jul 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+import optparse
+
+from scs_core.data.localized_datetime import LocalizedDatetime
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class CmdDeviceTopics(object):
+    """
+    unix command line handler
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.__parser = optparse.OptionParser(usage="%prog CLIENT_ID { -m MINUTES | -s START [-e END] } [-v]",
+                                              version="%prog 1.0")
+
+        # optional...
+        self.__parser.add_option("--minutes", "-m", type="int", nargs=1, action="store", dest="minutes",
+                                 help="starting minutes ago")
+
+        self.__parser.add_option("--start", "-s", type="string", nargs=1, action="store", dest="start",
+                                 help="localised datetime start")
+
+        self.__parser.add_option("--end", "-e", type="string", nargs=1, action="store", dest="end",
+                                 help="localised datetime end")
+
+        self.__parser.add_option("--verbose", "-v", action="store_true", dest="verbose", default=False,
+                                 help="report narrative to stderr")
+
+        self.__opts, self.__args = self.__parser.parse_args()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def is_valid(self):
+        if self.client_id is None or (self.__opts.start is None and self.minutes is None):
+            return False
+
+        if self.__opts.start is not None and LocalizedDatetime.construct_from_iso8601(self.__opts.start) is None:
+            return False
+
+        if self.__opts.end is not None and LocalizedDatetime.construct_from_iso8601(self.__opts.end) is None:
+            return False
+
+        return True
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def use_offset(self):
+        return self.minutes is not None
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def client_id(self):
+        return self.__args[0] if len(self.__args) > 0 else None
+
+
+    @property
+    def minutes(self):
+        return self.__opts.minutes
+
+
+    @property
+    def start(self):
+        return LocalizedDatetime.construct_from_iso8601(self.__opts.start) if self.__opts.start else None
+
+
+    @property
+    def end(self):
+        return LocalizedDatetime.construct_from_iso8601(self.__opts.end) if self.__opts.end else None
+
+
+    @property
+    def verbose(self):
+        return self.__opts.verbose
+
+
+    @property
+    def args(self):
+        return self.__args
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def print_help(self, file):
+        self.__parser.print_help(file)
+
+
+    def __str__(self, *args, **kwargs):
+        return "CmdDeviceTopics:{client_id:%s, minutes:%s, start:%s, end:%s, verbose:%s, args:%s}" % \
+                    (self.client_id, self.minutes, self.start, self.end, self.verbose, self.args)
